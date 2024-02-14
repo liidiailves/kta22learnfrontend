1584a750138d137c1d7888cbd5c0785ca96ce6f5
Liidia Ilves <liidia_ilves@hotmail.com>
2024-02-14 12:54:30 +0200

    Add ItemList example

---
+++ src/ItemList.vue
@@ -0,0 +1,13 @@
+<script setup>
+    defineProps(['title', 'items']);
+</script>
+
+<template>
+    <h1>{{title}}</h1>
+      <ul>
+        <li v-for="item in items" v-bind:key="item.id">
+          {{ item.name }}
+          <input type="checkbox" v-model="item.isDone">
+        </li>
+      </ul>
+</template>
